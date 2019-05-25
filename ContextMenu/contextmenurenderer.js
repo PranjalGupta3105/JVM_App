917c8ee17980f981d52b5b82cc7f71ba6647cfb8
Pranjal Gupta <guptapranjal3105@gmail.com>
2019-05-25 21:43:45 +0530

    Added Products Catalog[ImprovementNeeded]

--- ContextMenu/contextmenurenderer.js
+++ ContextMenu/contextmenurenderer.js
@@ -1,26 +1,30 @@
 const electron = require('electron');
 const ipc = electron.ipcRenderer;
 
+const Catalogbtn = document.getElementById('Catalogbtn');
+Catalogbtn.addEventListener('click', function(){
+
+    ipc.send('ShowCatalogPage');
+
+});
+
 const StockMgtbtn = document.getElementById('StckMgtbtn');
 StockMgtbtn.addEventListener('click', function(){
-    // var loginId = document.getElementById("UserName").value;
-    // var password = document.getElementById("Password").value;
+
     ipc.send('LoadStockManagementPage');
-    // AuthenticateUser(loginId,password)
+
 });
 
 const OrderMgtbtn = document.getElementById('OrdrMgtbtn');
 OrderMgtbtn.addEventListener('click', function(){
-    // var loginId = document.getElementById("UserName").value;
-    // var password = document.getElementById("Password").value;
+
     ipc.send('LoadOrderManagementPage');
-    // AuthenticateUser(loginId,password)
+
 });
 
 const TransactionMgtbtn = document.getElementById('TranscMgtbtn');
 TransactionMgtbtn.addEventListener('click', function(){
-    // var loginId = document.getElementById("UserName").value;
-    // var password = document.getElementById("Password").value;
+
     ipc.send('LoadTransactionManagementPage');
-    // AuthenticateUser(loginId,password)
+
 });
